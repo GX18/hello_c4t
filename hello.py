7aeff8f6aa78f082cebe9049baa5e0387a6b2908
GX18 <phambichan0505@gmail.com>
2019-06-07 21:46:33 +0700

    them file hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello World")
+n = int(input(" Nhap so n "))
+from random import randrange 
+m = int(randrange(1,100))
+print (m)
+
+if n=m :
+    print(" hai so bang nhau ")
+elif n<m :
+    print(" n be hon m ")
+else:
+    print(" n lon hon m ")
